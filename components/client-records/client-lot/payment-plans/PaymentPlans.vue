07000f7c8678fc036864df2250e3a13320871e50
vsalfafara <alfafara.vm@gmail.com>
2025-02-22 03:41:42 +0800

    Refactor fix

--- components/client-records/client-lot/payment-plans/PaymentPlans.vue
+++ components/client-records/client-lot/payment-plans/PaymentPlans.vue
@@ -154,9 +154,8 @@ const columns = [
     enableHiding: false,
     cell: ({ row }) => {
       const plan = row.original;
-      const disabled = plan.status === "Paid";
 
-      if (disabled) return null;
+      if (plan.status === "Paid") return null;
       return h(EditPaymentPlan, {
         paymentPlan: plan,
         onRefresh: handleGetPaymentPlans,
